173f2aff5003f757eb3c0be2ee064e06f5c2bcd5
nicolo.carpignoli <nicolo.carpignoli@chialab.it>
2019-08-29 17:29:43 +0200

    rotation

--- t/events.js
+++
@@ -1,9 +0,0 @@
-AFRAME.registerComponent('markerhandler', {
-    init: function() {
-        const entity = document.getElementById('entity');
-        if (entity) {
-            entity.addEventListener('model-loaded', function(){
-                alert('loaded')
-            });
-        }
-}});
